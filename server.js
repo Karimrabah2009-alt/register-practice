3ef4ab3a435548c11fbfaf329b4ee5d2356f4b36
Karim Rabah <karim.rabah2009@gmail.com>
2026-09-01 21:51:38 +0200

    Enable CSP and configure HSTS for production

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 app.use(
   helmet({
-    contentSecurityPolicy: false,
     strictTransportSecurity: isProduction ? undefined : false,
   })
 );
